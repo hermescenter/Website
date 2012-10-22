1d683b8cd4798639bb0b3d6cd37566794ed3344b
maretta <m.festari@thefool.it>
2012-10-22 11:35:02 +0200

    pagina donation

--- js/main.js
+++ js/main.js
@@ -2,5 +2,6 @@ Cufon.replace('#carosello h1', { fontFamily: 'Futura', textShadow: '#ffffff 1px 
 Cufon.replace('.box-home h2', { fontFamily: 'Futura' });
 Cufon.replace('.box-home-right h2', { fontFamily: 'Futura' });
 Cufon.replace('#content h1', { fontFamily: 'Futura' });
+Cufon.replace('#content h2', { fontFamily: 'Futura' });
 Cufon.replace('p.menu', { fontFamily: 'Futura' });
 
